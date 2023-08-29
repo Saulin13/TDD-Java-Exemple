a260c5d79e61bead8fc827214f90fbbea720896d
Saulo Pereira Vasconcelos <saulovasconcelos07@gmail.com>
2023-08-29 16:12:36 -0300

    Testes Relogio

--- Test/tdd/RelogioTest.java
+++ Test/tdd/RelogioTest.java
@@ -2,13 +2,43 @@ package tdd;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class RelogioTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testHora(){
+		
+		Relogio relogio = new Relogio();
+		
+		relogio.setHoras(10);
+		
+		 Assertions.assertEquals(10, relogio.getHoras());
+			
+	}
+	
+	@Test
+	public void testMinuto(){
+		
+		Relogio relogio = new Relogio();
+		
+		relogio.setMinutos(20);
+		
+		 Assertions.assertEquals(20, relogio.getMinutos());
+			
+	}
+	
+	@Test
+	public void testSegundo(){
+		
+		Relogio relogio = new Relogio();
+		
+		relogio.setSegundos(40);
+		
+		 Assertions.assertEquals(40, relogio.getSegundos());
+			
 	}
 
 }
+ 
